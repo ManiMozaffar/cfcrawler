4b4417d04025256acc646fd47842789fab88fa6c
Mani Mozaffar <mani.mozaffar@gmail.com>
2024-09-01 13:35:09 +0200

    Refactored entire code, making fake useragent optional (#2)
    * change poetryto uv
    
    * Refactor code base
    
    * Rewrite test to be more clear & be preceise about assumptions
    
    * Update uv
    
    * Last refactor with seperation of tests
    
    * Add some explanation to readme
    
    * Update tox & CD/CD
    
    * change match case to if else
    
    * Update gitignore and makefile
    
    * Change rye to uv in makefile
    
    * change github job name
    
    * Add py typed

---
+++ cfcrawler/tls.py
@@ -0,0 +1,75 @@
+import ssl
+import typing
+from functools import lru_cache
+
+import typing_extensions as te
+from httpcore import AsyncConnectionPool, AsyncHTTPProxy, AsyncSOCKSProxy
+
+from cfcrawler.types import Browser
+
+
+def mimic_tls_fingerprint_from_browser(
+    pool: typing.Union[AsyncConnectionPool, AsyncHTTPProxy, AsyncSOCKSProxy],
+    browser: Browser,
+    ecdh_curve: typing.Optional[str] = None,
+    cipher_suite: typing.Optional[str] = None,
+):
+    if pool._ssl_context is None:
+        pool._ssl_context = ssl.create_default_context()
+
+    ecdh_curve = ecdh_curve or "secp384r1"
+    cipher_suite = cipher_suite or get_cipher_suite(browser)
+    pool._ssl_context.set_ecdh_curve(ecdh_curve)
+    pool._ssl_context.set_ciphers(cipher_suite)
+    pool._ssl_context.check_hostname = False
+
+    pool._ssl_context.minimum_version = ssl.TLSVersion.TLSv1_2
+    pool._ssl_context.maximum_version = ssl.TLSVersion.TLSv1_3
+    pool._ssl_context = pool._ssl_context
+
+
+@lru_cache
+def get_cipher_suite(browser: Browser) -> str:
+    if browser == Browser.CHROME:
+        cipher_suites = [
+            "TLS_AES_128_GCM_SHA256",
+            "TLS_CHACHA20_POLY1305_SHA256",
+            "TLS_AES_256_GCM_SHA384",
+            "ECDHE-ECDSA-AES128-GCM-SHA256",
+            "ECDHE-RSA-AES128-GCM-SHA256",
+            "ECDHE-ECDSA-CHACHA20-POLY1305",
+            "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256",
+            "ECDHE-RSA-CHACHA20-POLY1305",
+            "ECDHE-RSA-AES256-GCM-SHA384",
+            "ECDHE-ECDSA-AES256-SHA",
+            "ECDHE-ECDSA-AES128-SHA",
+            "ECDHE-RSA-AES128-SHA",
+            "ECDHE-RSA-AES256-SHA",
+            "AES128-GCM-SHA256",
+            "AES256-GCM-SHA384",
+            "AES128-SHA",
+            "AES256-SHA",
+        ]
+    elif browser == Browser.FIREFOX:
+        cipher_suites = [
+            "TLS_AES_128_GCM_SHA256",
+            "TLS_CHACHA20_POLY1305_SHA256",
+            "TLS_AES_256_GCM_SHA384",
+            "ECDHE-ECDSA-AES128-GCM-SHA256",
+            "ECDHE-RSA-AES128-GCM-SHA256",
+            "ECDHE-ECDSA-CHACHA20-POLY1305",
+            "ECDHE-RSA-CHACHA20-POLY1305",
+            "ECDHE-ECDSA-AES256-GCM-SHA384",
+            "ECDHE-RSA-AES256-GCM-SHA384",
+            "ECDHE-ECDSA-AES256-SHA",
+            "ECDHE-ECDSA-AES128-SHA",
+            "ECDHE-RSA-AES128-SHA",
+            "ECDHE-RSA-AES256-SHA",
+            "AES128-GCM-SHA256",
+            "AES256-GCM-SHA384",
+            "AES128-SHA",
+            "AES256-SHA",
+        ]
+    else:
+        te.assert_never(browser)
+    return ":".join(cipher_suites)
--- tests/test_detect.py
+++ tests/test_detect.py
@@ -1,17 +1,30 @@
 import pytest
+from httpx import AsyncClient as DetectedClient
 
 from cfcrawler.client import AsyncClient
 from cfcrawler.types import Browser
 
 
 @pytest.mark.asyncio
-async def test_crawl():
+async def test_patched_async_client():
     client = AsyncClient(browser=Browser.CHROME)
-    resp = await client.get("https://www.cloudflare.com/")
+    resp = await client.get("https://www.futbin.com/")
     assert resp.status_code == 200
     assert "__cf_bm" in resp.cookies.keys()
 
     client = AsyncClient(browser=Browser.FIREFOX)
-    resp = await client.get("https://www.cloudflare.com/")
+    resp = await client.get("https://www.futbin.com/")
     assert resp.status_code == 200
     assert "__cf_bm" in resp.cookies.keys()
+
+    client = AsyncClient(browser=Browser.FIREFOX, use_fake_useragent_library=True)
+    resp = await client.get("https://www.futbin.com/")
+    assert resp.status_code == 200
+    assert "__cf_bm" in resp.cookies.keys()
+
+
+@pytest.mark.asyncio
+async def test_of_assumption_tht_target_is_still_protected():
+    client = DetectedClient()
+    resp = await client.get("https://www.futbin.com/")
+    assert resp.status_code != 200
